dd550d46b16b09e2ce9759eaf7f7d7c32324d359
mariuszpastuszka <mpastuszka@gmail.com>
2018-04-05 14:10:17 +0200

    [add] - adding hello world example

---
+++ src/main/java/pl/com/empas/java_introductory_course/base/HelloWorld.java
@@ -0,0 +1,8 @@
+package pl.com.empas.java_introductory_course.base;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
