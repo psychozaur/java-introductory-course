11de64078c33bc737f7bbfe193f41792fd7abf61
pfronc <fronc.piotr@gmail.com>
2018-04-07 22:42:55 +0200

    Add simple generic converter usage example

--- src/main/java/pl/com/empas/java_introductory_course/generics/converters/BooleanToStringConverter.java
+++ src/main/java/pl/com/empas/java_introductory_course/generics/converters/BooleanToStringConverter.java
@@ -2,6 +2,13 @@ package pl.com.empas.java_introductory_course.generics.converters;
 
 public class BooleanToStringConverter implements ConverterContract<Boolean, String> {
     public String convert(Boolean value) {
-        return value == null ? "false" : value.toString();
+        return value == null ? "null" : value.toString();
+    }
+
+    public static void main(String [] args) {
+        ConverterContract<Boolean, String> converter = new BooleanToStringConverter();
+        System.out.println(converter.convert(null));
+        System.out.println(converter.convert(true));
+        System.out.println(converter.convert(false));
     }
 }
