bd87a8df97e1ae8193b7be9edaaa79426e508d47
pfronc <fronc.piotr@gmail.com>
2018-04-17 17:55:28 +0200

    [change] legacy date timezone example

--- src/main/java/pl/com/empas/java_introductory_course/time/standard/DateExample.java
+++ src/main/java/pl/com/empas/java_introductory_course/time/standard/DateExample.java
@@ -2,6 +2,8 @@ package pl.com.empas.java_introductory_course.time.standard;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
 
 public class DateExample {
 
@@ -13,6 +15,17 @@ public class DateExample {
         System.out.println(now);
         System.out.println(future);
         System.out.println(now.compareTo(future));
+
+        TimeZone zoneCurrent = calendar.getTimeZone();
+        System.out.println("Current timezone: " + zoneCurrent.getDisplayName());
+        System.out.println("Current Zone Hour: " + calendar.get(Calendar.HOUR_OF_DAY));
+        TimeZone zoneLA = TimeZone.getTimeZone("America/Los_Angeles");
+        System.out.println("LA timezone offset: " + zoneLA.getOffset(calendar.getTimeInMillis()));
+        calendar.setTimeZone(zoneLA);
+        System.out.println("LA zone Hour: " + calendar.get(Calendar.HOUR_OF_DAY));
+
+
+
 
     }
 }
