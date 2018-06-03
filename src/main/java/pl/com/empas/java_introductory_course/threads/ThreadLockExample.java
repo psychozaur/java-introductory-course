5a783185b3d26e21b1026ca607cf22c041378860
mariuszpastuszka <mpastuszka@gmail.com>
2018-06-03 09:37:54 +0200

    [add] - example of thread locking

---
+++ src/main/java/pl/com/empas/java_introductory_course/threads/ThreadLockExample.java
@@ -0,0 +1,35 @@
+package pl.com.empas.java_introductory_course.threads;
+
+class Job implements Runnable {
+    private final Object resourceA, resourceB;
+
+    Job(Object resourceA, Object resourceB) {
+        this.resourceA = resourceA;
+        this.resourceB = resourceB;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(String.format("Job run by thread: [%s]", threadName));
+
+            System.out.println(String.format("Trying to synchronized on first resource by thread [%s]", threadName));
+            synchronized (resourceA) {
+                System.out.println(String.format("First resource acquired by thread [%s]", threadName));
+
+                System.out.println(String.format("Trying to synchronized on second resource by thread [%s]", threadName));
+                synchronized (resourceB) {
+                    System.out.println(String.format("Second resource acquired by thread [%s]", threadName));
+
+                    System.out.println(String.format("Working... on thread [%s]", threadName));
+                }
+            }
+
+            System.out.println(String.format("Releasing all resources by thread [%s]", threadName));
+        }
+    }
+}
+
+public class ThreadLockExample {
+}
